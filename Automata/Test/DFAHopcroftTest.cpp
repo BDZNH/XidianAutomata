d229aef18fcb3397c55d3dac3e19c9c99cf3ee44
BDZNH <pxezhsp@live.com>
2019-05-07 23:28:47 +0800

    Update DFAHopcroftTest.cpp

--- Automata/Test/DFAHopcroftTest.cpp
+++ Automata/Test/DFAHopcroftTest.cpp
@@ -17,6 +17,9 @@ void DFAHopcroftTest()
 		DFA dfa1(dfa_com);
 		std::cout << "Original DFA:" << std::endl;
 		std::cout << dfa1 << std::endl;
+		dfa1.usefulf();
+		std::cout << "after usefulf():" << std::endl ;
+		std::cout << dfa1 << std::endl << std::endl ;
 		dfa1.min_Hopcroft();
 		std::cout << "Minimized by Hopcroft:" << std::endl;
 		std::cout << dfa1 << std::endl;
@@ -29,6 +32,7 @@ void DFAHopcroftTest()
 DFA_components DfaCom(int n)
 {
 	DFA_components dfa_com1;
+	int i;
 	switch (n)
 	{
 	case 1:
@@ -40,7 +44,7 @@ DFA_components DfaCom(int n)
 		状态说明    状态     输入字符
 							a      b
         开始状态     q0      q1     q2
-		             q1      q3     
+		            q1      q3     
 		结束状态     q2             q3
 		结束状态     q3      q4
 		结束状态     q4
@@ -51,24 +55,35 @@ DFA_components DfaCom(int n)
 
 		*/
 		/// 此例子 Hopcroft 中止
+
+		i = 6;
+		while (i--)
+		{
+			dfa_com1.Q.allocate();
+		}
+
 		// StateSet S  开始状态集
-		dfa_com1.S.set_domain(5);
+		dfa_com1.S.set_domain(dfa_com1.Q.size());
 		dfa_com1.S.add(0);
 
 		// StateSet F  结束状态集
-		dfa_com1.F.set_domain(5);
+		dfa_com1.F.set_domain(dfa_com1.Q.size());
 		dfa_com1.F.add(2);
 		dfa_com1.F.add(3);
 		dfa_com1.F.add(4);
 
-		dfa_com1.Q.set_domain(5);
-
-		dfa_com1.T.set_domain(5);
+		dfa_com1.T.set_domain(dfa_com1.Q.size());
 		dfa_com1.T.add_transition(0, 'a', 1);
 		dfa_com1.T.add_transition(0, 'b', 2);
 		dfa_com1.T.add_transition(1, 'a', 3);
 		dfa_com1.T.add_transition(2, 'b', 3);
 		dfa_com1.T.add_transition(3, 'b', 4);
+
+		// for complete automata
+		//dfa_com1.T.add_transition(1, 'b', 5);
+		//dfa_com1.T.add_transition(2, 'a', 5);
+		//dfa_com1.T.add_transition(3, 'a', 5);
+
 	}
 	break;
 	case 2:
@@ -86,23 +101,30 @@ DFA_components DfaCom(int n)
 		预期结果    本身为最小的 DFA
 
 		*/
-
+		i = 4;
+		while (i--)
+		{
+			dfa_com1.Q.allocate();
+		}
 		// StateSet S  开始状态集
-		dfa_com1.S.set_domain(3);
+		dfa_com1.S.set_domain(dfa_com1.Q.size());
 		dfa_com1.S.add(0);
 
 		// StateSet F  结束状态集
-		dfa_com1.F.set_domain(3);
+		dfa_com1.F.set_domain(dfa_com1.Q.size());
 		dfa_com1.F.add(1);
 		dfa_com1.F.add(2);
 
-		dfa_com1.Q.set_domain(3);
 
-		dfa_com1.T.set_domain(3);
+		dfa_com1.T.set_domain(dfa_com1.Q.size());
 		dfa_com1.T.add_transition(0, 'a', 1);
 		dfa_com1.T.add_transition(0, 'b', 2);
 		dfa_com1.T.add_transition(1, 'b', 2);
 		dfa_com1.T.add_transition(2, 'a', 1);
+
+		// for complete automata
+		//dfa_com1.T.add_transition(1, 'a', 3);
+		//dfa_com1.T.add_transition(2, 'b', 3);
 
 	}
 	break;
@@ -123,18 +145,23 @@ DFA_components DfaCom(int n)
 
 		*/
 		// 此例Hopcroft正确 
+
+		i = 4;
+		while (i--)
+		{
+			dfa_com1.Q.allocate();
+		}
+
 		// StateSet S  开始状态集
-		dfa_com1.S.set_domain(4);
+		dfa_com1.S.set_domain(dfa_com1.Q.size());
 		dfa_com1.S.add(0);
 
 		// StateSet F  结束状态集
-		dfa_com1.F.set_domain(4);
+		dfa_com1.F.set_domain(dfa_com1.Q.size());
 		dfa_com1.F.add(2);
 		dfa_com1.F.add(3);
 
-		dfa_com1.Q.set_domain(4);
-
-		dfa_com1.T.set_domain(4);
+		dfa_com1.T.set_domain(dfa_com1.Q.size());
 		dfa_com1.T.add_transition(0, 'a', 1);
 		dfa_com1.T.add_transition(1, 'b', 2);
 		dfa_com1.T.add_transition(2, 'a', 3);
@@ -148,33 +175,43 @@ DFA_components DfaCom(int n)
 
 		状态说明    状态     输入字符
 							a      b
-		开始状态	q0      q1
-					q1             q2
-		结束状态	q2      q3
-		结束状态	q3             q4
+		开始状态	q0         q1
+				q1                q2
+		结束状态	q2         q3
+		结束状态	q3                q4
 		结束状态	q4
 		预期结果    本身为最小的 DFA
 
 		*/
 		// 此例子 Hopcroft 中止
+
+		i = 6;
+		while (i--)
+		{
+			dfa_com1.Q.allocate();
+		}
+
 		// StateSet S  开始状态集
-		dfa_com1.S.set_domain(5);
+		dfa_com1.S.set_domain(dfa_com1.Q.size());
 		dfa_com1.S.add(0);
 
 		// StateSet F  结束状态集
-		dfa_com1.F.set_domain(5);
+		dfa_com1.F.set_domain(dfa_com1.Q.size());
 		dfa_com1.F.add(2);
 		dfa_com1.F.add(3);
 		dfa_com1.F.add(4);
-		//dfa_com1.F.add(6);
 
-		dfa_com1.Q.set_domain(5);
-
-		dfa_com1.T.set_domain(5);
+		dfa_com1.T.set_domain(dfa_com1.Q.size());
 		dfa_com1.T.add_transition(0, 'a', 1);
 		dfa_com1.T.add_transition(1, 'b', 2);
 		dfa_com1.T.add_transition(2, 'a', 3);
 		dfa_com1.T.add_transition(3, 'b', 4);
+
+		// for complete automata
+		//dfa_com1.T.add_transition(0, 'b', 5);
+		//dfa_com1.T.add_transition(1, 'a', 5);
+		//dfa_com1.T.add_transition(2, 'b', 5);
+		//dfa_com1.T.add_transition(3, 'a', 5);
 	}
 	break;
 	case 5:
@@ -185,29 +222,33 @@ DFA_components DfaCom(int n)
 
 		状态说明    状态     输入字符
 							a      b
-		开始状态	q0      q1
+		开始状态	    q0      q1
 					q1             q2
 					q2      q3
-		结束状态	q3             q4
-		结束状态	q4
+		结束状态	    q3             q4
+		结束状态	   q4
 		预期结果    本身为最小的 DFA
 
 		*/
 		// 此例 Hopcroft 正确
+
+		i = 5;
+		while (i--)
+		{
+			dfa_com1.Q.allocate();
+		}
+
 		// StateSet S  开始状态集
-		dfa_com1.S.set_domain(5);
+		dfa_com1.S.set_domain(dfa_com1.Q.size());
 		dfa_com1.S.add(0);
 
 		// StateSet F  结束状态集
-		dfa_com1.F.set_domain(5);
-		//dfa_com1.F.add(2);
+		dfa_com1.F.set_domain(dfa_com1.Q.size());
 		dfa_com1.F.add(3);
 		dfa_com1.F.add(4);
-		//dfa_com1.F.add(6);
 
-		dfa_com1.Q.set_domain(5);
 
-		dfa_com1.T.set_domain(5);
+		dfa_com1.T.set_domain(dfa_com1.Q.size());
 		dfa_com1.T.add_transition(0, 'a', 1);
 		dfa_com1.T.add_transition(1, 'b', 2);
 		dfa_com1.T.add_transition(2, 'a', 3);
